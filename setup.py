010211cf63443a9391ba4ccec602a1f7bbe4b35d
Matt Michie <mattm@amazon.com>
2013-03-25 11:10:50 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sparkback',
+      version='0.0',
+      description='Sparklines Library',
+      author='Matt Michie',
+      author_email='mmichie@gmail.com',
+      url='https://github.com/mmichie/sparkback',
+      packages=['sparkback',],
+     )

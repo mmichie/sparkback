08356fae20fb1c932b5f6d8485cb56a82568de17
Matt Michie <mattm@amazon.com>
2013-03-26 15:45:10 -0700

    Have scale_data return generators instead of lists

--- sparkback/__init__.py
+++ sparkback/__init__.py
@@ -11,9 +11,9 @@ def scale_data(data, ticks):
     # if every element is the same height return all lower ticks, else compute
     # the tick height
     if n == 0: 
-        return [ ticks[0] for t in data]
+        return ( ticks[0] for t in data )
     else: 
-        return [ ticks[int((t - m) / n)] for t in data ]
+        return ( ticks[int((t - m) / n)] for t in data )
 
 def print_ansi_spark(d):
     print ''.join(d)
